a84069d68a3af92c638eccd437dfa35e95186cef
OmarMuita <omarmuita@gmail.com>
2024-12-12 17:23:25 +0300

    Add files via upload

---
+++ LIST LESSON.py
@@ -0,0 +1,12 @@
+emptylist=[]
+print()
+
+Numberlist=[1,2,3,4,5,6,7,8,9]
+print(Numberlist)
+
+multiplylist=[1,2,3,4,5]*5
+print(multiplylist)
+
+alist=[100,200,300,400,500]
+alist=alist[::-1]
+print(alist,"\n")
